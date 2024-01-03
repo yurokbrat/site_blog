19a9edc3a33718e407c905e86fd74183af473d84
yurokbrat <yurokbrat@yandex.ru>
2024-01-03 23:07:30 +0300

    Create profiles, use image and CRUD

--- site_web/blog/urls.py
+++ site_web/blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog_home'),
+    path('', views.PostListView.as_view(), name='blog_home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', views.PostDeleteViews.as_view(), name='post_delete'),
     path('about/', views.about, name='blog_about'),
 ]
--- site_web/users/forms.py
+++ site_web/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -10,7 +11,7 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-    username = forms.CharField(label='Имя пользователя',
+    username = forms.CharField(label='Логин пользователя',
                                help_text='Только буквы, цифры и @/./+/-/_')
     email = forms.EmailField(label='Адрес электронной почты')
     password1 = forms.CharField(label='Пароль',
@@ -20,15 +21,32 @@ class UserRegistrationForm(UserCreationForm):
     password2 = forms.CharField(label='Подтверждение пароля', help_text='Введите тот же самый пароль для проверки.')
 
     error_messages = {
-            'password_mismatch': 'Пароли не совпадают.',
-            'username_exists': 'Пользователь с таким именем уже существует.',
-            'invalid_username': 'Недопустимое имя пользователя. Используйте только буквы, цифры и символы @/./+/-/_',
-            'password_too_common': 'Слишком лёгкий пароль. Выберите более сложный пароль.',
-            'password_numeric_only': 'Пароль не может состоять только из цифр.',
-            'password_too_short': 'Пароль слишком короткий. Минимальная длина - 8 символов.',
-            'password_too_long': 'Пароль слишком длинный. Максимальная длина - 150 символов.',
-            'invalid_email': 'Некорректный адрес электронной почты.',
-            'email_exists': 'Пользователь с таким адресом электронной почты уже существует.',
-            'password_numeric_only': 'Пароль не может состоять только из цифр.',
-        }
+        'password_mismatch': 'Пароли не совпадают.',
+        'username_exists': 'Пользователь с таким именем уже существует.',
+        'invalid_username': 'Недопустимое имя пользователя. Используйте только буквы, цифры и символы @/./+/-/_',
+        'password_too_common': 'Слишком лёгкий пароль. Выберите более сложный пароль.',
+        'password_numeric_only': 'Пароль не может состоять только из цифр.',
+        'password_too_short': 'Пароль слишком короткий. Минимальная длина - 8 символов.',
+        'password_too_long': 'Пароль слишком длинный. Максимальная длина - 150 символов.',
+        'invalid_email': 'Некорректный адрес электронной почты.',
+        'email_exists': 'Пользователь с таким адресом электронной почты уже существует.',
+    }
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    username = forms.CharField(label='Логин пользователя',
+                               help_text='Только буквы, цифры и @/./+/-/_')
+    email = forms.EmailField(label='Адрес электронной почты')
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+        labels = {'image': 'Фото профиля'}
