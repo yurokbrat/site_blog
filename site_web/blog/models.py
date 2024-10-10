2199fa6112ea4f9b75243d755e56b88e3a3621f6
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-10-10 11:11:42 +0300

    Add verbose names

--- site_web/blog/models.py
+++ site_web/blog/models.py
@@ -8,8 +8,14 @@ from uuid import uuid4
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="заголовок")
     content = models.TextField(verbose_name="контент")
-    data_posted = models.DateTimeField(default=timezone.now, verbose_name="дата публикации")
+    data_posted = models.DateTimeField(
+        default=timezone.now, verbose_name="дата публикации"
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="автор")
+
+    class Meta:
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
 
     def __str__(self):
         return self.title
@@ -24,6 +30,10 @@ class Comment(models.Model):
     text = models.TextField(verbose_name="текст")
     created = models.DateTimeField(auto_now_add=True, verbose_name="создано в")
     id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
+
+    class Meta:
+        verbose_name = "комментарий"
+        verbose_name_plural = "комментарии"
 
     def __str__(self):
         return f"Comment by {self.author.username} to {self.post.title}"
--- site_web/users/models.py
+++ site_web/users/models.py
@@ -7,6 +7,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
+    class Meta:
+        verbose_name = "профиль"
+        verbose_name_plural = "профили"
+
     def __str__(self):
         return f"{self.user.username} Profile"
 
